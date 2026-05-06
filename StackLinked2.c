b4b01d974c2080db68ccb26d021e10e3f73fcd73
Dipanjan Sadhukhan <dipanjansadhukhan123@gmail.com>
2026-05-06 20:57:53 +0530

    changes

--- StackLinked2.c
+++ StackLinked2.c
@@ -1,70 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct stack
+
+struct Stack
 {
     int size;
     int top;
     int *arr;
 };
-int isFull(struct stack *ptr)
+
+struct Stack *createStack(int size)
 {
-    if (ptr->top == ptr->size - 1)
-        return 1;
-    return 0;
-}
-int isEmpty(struct stack *ptr)
-{
-    if (ptr->top == -1)
-        return 1;
-    return 0;
-}
-int push(struct stack *ptr, int val)
-{
-    if (isFull(ptr))
+    struct Stack *s = malloc(sizeof(struct Stack));
+
+    if (s == NULL)
     {
-        printf("STACK OVERFLOW!");
-        return -1;
+        printf("Memory Allocation Failed\n");
+        exit(1);
     }
-    else
-    {
-        ptr->top++;
-        ptr->arr[ptr->top] = val;
-    }
-}
-int pop(struct stack *ptr)
-{
-    if (isEmpty(ptr))
-    {
-        printf("Stack Underflow!");
-        return -1;
-    }
-    else
-    {
-        int val = ptr->arr[ptr->top];
-        ptr->top = ptr->top - 1;
-        return val;
-    }
-}
-int peek(struct stack *ptr, int i)
-{
-    if (ptr->top - i + 1 < 0)
-        printf("Invalid Index");
-    else
-        return ptr->arr[ptr->top - i + 1];
-}
-int main(void)
-{
-    struct stack *s = (struct stack *)malloc(sizeof(struct stack));
-    s->size = 12;
+
+    s->size = size;
     s->top = -1;
-    s->arr = (int *)malloc(s->size * sizeof(int));
-    for (int i = 1; i <= s->size; i++)
-        push(s, i * i + (2 + i));
-    printf("Popped: %d\n", pop(s));
-    printf("Peeked element at index 6: %d\n\nElements at-\n", peek(s, 6));
-    for (int i = s->top; i >= 0; i--)
-        printf("Index %d: %d\n", i + 1, peek(s, i));
-    free(s);
+    s->arr = malloc(size * sizeof(int));
+
+    if (s->arr == NULL)
+    {
+        printf("Memory Allocation Failed\n");
+        exit(1);
+    }
+
+    return s;
+}
+
+void destroyStack(struct Stack *s)
+{
     free(s->arr);
+    free(s);
+}
+
+int isFull(struct Stack *s)
+{
+    return (s->top == s->size - 1);
+}
+
+int isEmpty(struct Stack *s)
+{
+    return (s->top == -1);
+}
+
+void push(struct Stack *s, int val)
+{
+    if (isFull(s))
+    {
+        printf("Stack Overflow!\n");
+        return;
+    }
+
+    s->arr[++(s->top)] = val;
+}
+
+int pop(struct Stack *s)
+{
+    if (isEmpty(s))
+    {
+        printf("Stack Underflow!\n");
+        return -1;
+    }
+
+    return s->arr[(s->top)--];
+}
+
+int peek(struct Stack *s, int position)
+{
+    int index = s->top - position + 1;
+
+    if (index < 0)
+    {
+        printf("Invalid Position\n");
+        return -1;
+    }
+
+    return s->arr[index];
+}
+
+void displayStack(struct Stack *s)
+{
+    printf("\nElements in stack:\n");
+
+    for (int pos = 1; pos <= s->top + 1; pos++)
+    {
+        printf("Position %d : %d\n", pos, peek(s, pos));
+    }
+}
+
+int main()
+{
+    struct Stack *s = createStack(5);
+
+    for (int i = 1; i <= 5; i++)
+        push(s, i * i + (i + 2));
+
+    printf("Popped: %d\n", pop(s));
+
+    printf("Element at position 2: %d\n",
+           peek(s, 2));
+
+    displayStack(s);
+
+    destroyStack(s);
+
     return 0;
 }
